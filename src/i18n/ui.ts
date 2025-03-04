54c38e90ad89fa46503318a7e20b5320b14d5293
alvarolc01 <alvaroluquecu@gmail.com>
2025-03-04 22:06:23 +0100

    feat: added language button

---
+++ src/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  cat: 'Català'
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'landing.viewWork': 'View my work',
+    'landing.letsConnect': "Let's connect!",
+  },
+  es: {
+    'landing.viewWork': 'Ver mi trabajo',
+    'landing.letsConnect': 'Conectemos!',
+  },
+  cat: {
+    'landing.viewWork': 'Veure el meu treball',
+    'landing.letsConnect': 'Connectem!',
+  },
+} as const;
