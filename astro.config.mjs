dfd5491bbf5205a076a57b9ba0d6dc6993cca3e4
alvarolc01 <alvaroluquecu@gmail.com>
2025-03-05 18:26:18 +0100

    fix: update i18n astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'fr'],
+    routing: {
+      prefixDefaultLocale: false
+    }
   }
 });
